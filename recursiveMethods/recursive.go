45f2bc60168ee8a8b9a098e664dcd5783d29c170
Crisi <harmaharcri@hotmail.com>
2020-09-12 11:11:35 -0500

    Added functional functions and recursive funcions

---
+++ recursiveMethods/recursive.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	anonimus := func(x, y int) int { return x + y }
+	number := anonimus(1, 1)
+	fmt.Println(number)
+	number2 := func(x, y float64) float64 { return math.Pow(x, y) }(2, 3)
+	fmt.Println(number2)
+	fv := func() { fmt.Println("Hello functional World") }
+	fmt.Println(fv)     //It gives the address
+	fmt.Println(test()) //It return 2 because yes, well actually because de defer is executed after the return value so 1+1 = 2
+	fun := returningLambdas()
+	another := fun(3)
+	fmt.Println(another, " function inside of a function")
+	for i := 0; i < 10; i++ {
+		fmt.Println(i, " ", fibonacci(i))
+	}
+	fmt.Println("Now with lambdas")
+	fibo := fibonacciLambda()
+	for i := 0; i < 10; i++ {
+		fmt.Println(i, " ", fibo(i))
+	}
+
+}
+
+func fibonacciLambda() func(number int) int {
+	return func(number int) int {
+		res := 1
+		if number > 1 {
+			res = 0
+			a := 1
+			b := 1
+			for i := 2; i <= number; i++ {
+				res = a + b
+				a = b
+				b = res
+			}
+		}
+		return res
+	}
+}
+
+func test() (ret int) {
+	defer func() { ret++ }()
+	return 1
+}
+func returningLambdas() func(b int) int {
+	return func(b int) int { return b * b }
+}
+
+func Fibonacci(i int) (int, int) {
+	if i <= 1 {
+		return i, 1
+	} else {
+		number, _ := Fibonacci(i - 2)
+		number2, _ := Fibonacci(i - 1)
+		return number + number2, i
+	}
+}
+
+func fibonacci(i int) (res int) {
+	if i <= 1 {
+		res = 1
+	} else {
+		res = fibonacci(i-2) + fibonacci(i-1)
+	}
+	return
+}
+
+func Factorial(number int64) (res int64) {
+	if number == 0 {
+		res = 1
+	} else {
+		res = number * Factorial(number-1)
+	}
+	return
+}
--- statements/state.go
+++ statements/state.go
@@ -1,11 +1,44 @@
-package main
+package statements
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println(isMajor(1, 1))
+	number, err := MySqrt(-16.0)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(number)
+	Greeting("Hola familia ", "Papi")
+	DatabaseOperations()
 
 }
+func Greeting(message string, people ...string) {
+
+	if len(people) == 1 {
+		fmt.Println(message, " ", people[0])
+	} else {
+		for _, val := range people {
+			fmt.Println(message, val)
+		}
+	}
+
+}
+func CloseDatabaseConnection() {
+	fmt.Println("Closing connections")
+}
+func DatabaseOperations() {
+	defer CloseDatabaseConnection()
+	fmt.Println("Connections to Database")
+	fmt.Println("Connected to Database succesfull")
+	fmt.Println("Using Database ")
+	return
+	//Defer is executed before the return statement
+}
+
 func isMajor(a int, b int) bool {
 LABEL1:
 	switch {
@@ -17,6 +50,13 @@ LABEL1:
 	}
 
 	return false
+}
+func MySqrt(number float64) (float64, error) {
+	if number < 0 {
+		return 0, errors.New("Negative number, this should not be")
+	}
+	var half float64 = 1.0 / 2.0
+	return math.Pow(number, half), nil
 }
 
 func triangle(n int, pointer *string) {
